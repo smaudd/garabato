5e9782b91a0ce5003b62babea6de23cc3cfa37e7
Santiago <smarcanoweb@gmail.com>
2026-03-26 17:09:20 +0100

    comment controller tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,47 +2,33 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    sign_in_as(@user)
+    @post = posts(:one)
     @comment = comments(:one)
-  end
-
-  test "should get index" do
-    get comments_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_comment_url
-    assert_response :success
   end
 
   test "should create comment" do
     assert_difference("Comment.count") do
-      post comments_url, params: { comment: { body: @comment.body, post_id: @comment.post_id } }
+      post post_comments_path(@post), params: { comment: { body: "Nuevo comentario", user_id: @user.id } }
     end
-
-    assert_redirected_to comment_url(Comment.last)
+    assert_redirected_to post_path(@post)
   end
 
-  test "should show comment" do
-    get comment_url(@comment)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_comment_url(@comment)
+  test "should edit comment" do
+    get edit_post_comment_path(@post, @comment)
     assert_response :success
   end
 
   test "should update comment" do
-    patch comment_url(@comment), params: { comment: { body: @comment.body, post_id: @comment.post_id } }
-    assert_redirected_to comment_url(@comment)
+    patch post_comment_path(@post, @comment), params: { comment: { body: "Editado" } }
+    assert_redirected_to post_path(@post)
   end
 
   test "should destroy comment" do
     assert_difference("Comment.count", -1) do
-      delete comment_url(@comment)
+      delete post_comment_path(@post, @comment)
     end
-
-    assert_redirected_to comments_url
+    assert_redirected_to post_path(@post)
   end
 end
