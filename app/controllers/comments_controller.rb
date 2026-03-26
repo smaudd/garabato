b662cab2f98ab836f1659280d7d1e0d1e9525b58
Santiago <smarcanoweb@gmail.com>
2026-03-26 16:17:53 +0100

    feat: initial auth with posts+comments code base

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  before_action :set_comment, only: %i[ edit update destroy ]
+
+  def edit
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = Current.user if defined?(Current) && Current.respond_to?(:user)
+    if @comment.save
+      redirect_to @post, notice: "Comment was successfully created."
+    else
+      render "posts/show", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @post, notice: "Comment was successfully updated."
+    else
+      redirect_to post_path(@post, edit_comment_id: @comment.id), alert: @comment.errors.full_messages.to_sentence
+    end
+  end
+
+  def destroy
+    @comment.destroy!
+    redirect_to @post, notice: "Comment was successfully destroyed."
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = @post.comments.find(params[:id])
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.expect(comment: [ :post_id, :body ])
+    end
+end
