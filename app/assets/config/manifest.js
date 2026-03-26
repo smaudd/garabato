0bb62e8c05541e45bf1e26ee875d485630a3f94e
Santiago <smarcanoweb@gmail.com>
2026-03-26 18:06:25 +0100

    add sass propshaft support

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
